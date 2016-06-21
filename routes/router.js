f3f5bad88bfbf173b9bee4c6ddf363bd9e103ec3
Leonard Schuetz <leni.schuetz@me.com>
2016-06-21 10:42:41 +0200

    Fixed survey and routing

--- routes/router.js
+++ routes/router.js
@@ -3,8 +3,9 @@ const router    = express.Router({ strict: true });
 const path      = require('path');
 const fs        = require('fs');
 
+// Main routes
 router.use('/:section', (req, res, next) => {
-    const pages = ['umfrage', 'vergleich', 'interview', 'ueberuns'];
+    const pages = ['home', 'umfrage', 'vergleich', 'interview', 'ueberuns'];
     if (pages.indexOf(req.params.section) > -1) {
         res.render(req.params.section, {
             ['current_' + req.params.section]: true,
@@ -14,6 +15,7 @@ router.use('/:section', (req, res, next) => {
     }
 });
 
+// For the index route, render the home page
 router.use(/\/$/, (req, res) => {
     res.render('home', {
         current_home: true,
